62469769ea21f8898a8fddab672a15dff5a05c7b
ifsheldon <maplevantablack@outlook.com>
2020-10-31 02:09:07 +0800

    added dependencies and new code for connecting to server

---
+++ app/src/main/java/com/example/caption/networking/ServiceClient.java
@@ -0,0 +1,160 @@
+package com.example.caption.networking;
+
+import android.util.Log;
+import android.widget.ArrayAdapter;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.messaging.simp.stomp.StompFrameHandler;
+import org.springframework.messaging.simp.stomp.StompHeaders;
+import org.springframework.messaging.simp.stomp.StompSession;
+import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.web.socket.WebSocketHttpHeaders;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+import org.springframework.web.socket.sockjs.client.Transport;
+import org.springframework.web.socket.sockjs.client.WebSocketTransport;
+import org.springframework.web.socket.sockjs.frame.Jackson2SockJsMessageCodec;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Created by yi-ge
+ * 2018-12-21 23:46
+ */
+class ServiceClient implements Runnable
+{
+    private static class MyHandler extends StompSessionHandlerAdapter
+    {
+        public void afterConnected(StompSession stompSession, StompHeaders stompHeaders)
+        {
+            Log.v(LOG_VERBOSE, "Now connected");
+        }
+    }
+
+    private class ClientStompFrameHandler implements StompFrameHandler
+    {
+        final ObjectMapper mapper = new ObjectMapper();
+
+        public Type getPayloadType(StompHeaders stompHeaders)
+        {
+            return byte[].class;
+        }
+
+        public void handleFrame(StompHeaders stompHeaders, Object o)
+        {
+            Log.v(LOG_VERBOSE, "Received greeting " + new String((byte[]) o));
+            try
+            {
+                Map<String, Object> m1 = mapper.readValue(new String((byte[]) o), Map.class);
+                Log.v(LOG_VERBOSE, m1.toString());
+                String eventName = ((Map<String, Object>) m1.get("header")).get("name").toString();
+                // get subtitles here
+                String text = ((Map<String, Object>) m1.get("payload")).get("result").toString();
+
+                if ("TranscriptionResultChanged".equals(eventName))
+                {
+                    subtitleStorage.add(text, false);
+                } else if ("SentenceEnd".equals(eventName))
+                {
+                    subtitleStorage.add(text, true);
+                }
+                //TODO: check whether it works
+                arrayAdapter.add(subtitleStorage.get());
+            }
+            catch (IOException e)
+            {
+                Log.e(LOG_DEBUG, e.getMessage());
+            }
+        }
+
+    }
+
+    private static final String LOG_VERBOSE = "Client VERBOSE";
+    private static final String LOG_DEBUG = "Client DEBOG";
+    private final ArrayAdapter<String> arrayAdapter;
+    private final SubtitleStorage subtitleStorage = new SubtitleStorage();
+    private final static WebSocketHttpHeaders headers = new WebSocketHttpHeaders();
+    private final AtomicBoolean stop = new AtomicBoolean(false);
+
+    public ServiceClient(ArrayAdapter<String> arrayAdapter)
+    {
+        this.arrayAdapter = arrayAdapter;
+    }
+
+
+    public ListenableFuture<StompSession> connect()
+    {
+
+        Transport webSocketTransport = new WebSocketTransport(new StandardWebSocketClient());
+        List<Transport> transports = Collections.singletonList(webSocketTransport);
+
+        SockJsClient sockJsClient = new SockJsClient(transports);
+        sockJsClient.setMessageCodec(new Jackson2SockJsMessageCodec());
+
+        WebSocketStompClient stompClient = new WebSocketStompClient(sockJsClient);
+
+        String url = "ws://{host}:{port}/gs-guide-websocket";
+        // TODO: notice this special path for Android virtual device
+        String webPath = "10.0.2.2";
+        return stompClient.connect(url, headers, new MyHandler(), webPath, 23330);
+    }
+
+    public void subscribeGreetings(StompSession stompSession) throws ExecutionException, InterruptedException
+    {
+        //TODO: change the topic
+        stompSession.subscribe("/topic/test", new ClientStompFrameHandler());
+        Log.v(LOG_VERBOSE, "subscribe done");
+    }
+
+    public void stop()
+    {
+        stop.set(true);
+    }
+
+    @Override
+    public void run()
+    {
+        ListenableFuture<StompSession> f = this.connect();
+        StompSession stompSession = null;
+        try
+        {
+            stompSession = f.get();
+        }
+        catch (InterruptedException | ExecutionException e)
+        {
+            Log.e(LOG_DEBUG, e.getMessage());
+        }
+
+        Log.v(LOG_VERBOSE, "Subscribing to topic using session " + stompSession);
+        try
+        {
+            this.subscribeGreetings(stompSession);
+        }
+        catch (ExecutionException | InterruptedException e)
+        {
+            Log.e(LOG_DEBUG, e.getMessage());
+        }
+//        new Scanner(System.in).nextLine();
+        while (!stop.get())
+        {
+            try
+            {
+                Thread.sleep(100);
+            }
+            catch (InterruptedException ignored)
+            {
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/caption/networking/SubtitleStorage.java
@@ -0,0 +1,212 @@
+package com.example.caption.networking;
+
+import org.apache.commons.lang3.tuple.MutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+class SubtitleStorage
+{
+
+    int maxLines = 10;
+    int currentSentence = 0;
+    List<List<Pair<Integer, Integer>>> wordNumbersOnScreen = new ArrayList<>(maxLines);
+    List<List<String>> linesOnScreen = new ArrayList<>(maxLines);
+    boolean lastSentenceFinished = false;
+    String lastSentence = "";
+    String line1 = "";
+    String line2 = "";
+    List<Double> pastSpeed = new ArrayList<>(maxLines);
+    long currentBegin = 0L;
+    double currentSpeed = 0.0;
+    long startTime = 0L;
+    int totalWordCount = 0;
+
+    public double getCurrentSpeed() {
+        return currentSpeed;
+    }
+
+    public double getMovingAvgSpeed() {
+        return pastSpeed.stream().mapToDouble(Double::doubleValue).average().orElse(0);
+    }
+
+    public String getStat(){
+        int timePassed = (int)((System.currentTimeMillis() - startTime) / 1000L);
+        GregorianCalendar cal = new GregorianCalendar(0,0,0,0,0,timePassed);
+        Date dNow = cal.getTime();
+        SimpleDateFormat ft = new SimpleDateFormat("HH:mm:ss");
+        return String.format("Caption - Time: %s, Words: %d, Sentences: %d, WPM: %.1f, AVG: %.1f", ft.format(dNow), totalWordCount, currentSentence, currentSpeed, getMovingAvgSpeed());
+    }
+
+    public SubtitleStorage() {
+        // 简单初始化，避免后面的复杂判断
+        linesOnScreen.add(new ArrayList<>());
+        linesOnScreen.add(new ArrayList<>());
+        wordNumbersOnScreen.add(new ArrayList<>());
+        wordNumbersOnScreen.add(new ArrayList<>());
+
+    }
+
+    public static boolean isLineLegal(List<String> line) {
+        // 两个每行的限制
+        int maxWordPerLine = 100;
+        int maxLineLength = 50;
+
+        if(line.size()>maxWordPerLine) return false;
+        int totalLength = line.stream().mapToInt(String::length).sum() + line.size()-1 ;
+        if(totalLength>maxLineLength) return false;
+
+        return true;
+    }
+
+    public void add(String tr, boolean sentenceFinished){
+        System.out.println("tr="+tr+" finish"+sentenceFinished);
+
+        if(currentBegin==0){
+            currentBegin = System.currentTimeMillis();
+        }
+        if(startTime==0){
+            startTime = System.currentTimeMillis();
+        }
+
+        boolean isNewSentence = (lastSentenceFinished);
+        lastSentenceFinished = sentenceFinished;
+
+        System.out.println("isNew="+isNewSentence);
+
+        if(isNewSentence){
+            lastSentence = "";
+            currentSentence++;
+            currentBegin = System.currentTimeMillis();
+            if(pastSpeed.size()>maxLines) pastSpeed.remove(0);
+        }
+
+        // 移除标点符号（可选开放？）
+        // 阿里云 API 的限制
+        tr = tr.replaceAll("[,.!?\\\\-]", " ");
+
+        String[] currentSentenceWords = tr.split("\\s+");
+        String[] lastSentenceWords = lastSentence.split("\\s+");
+
+        totalWordCount = totalWordCount - lastSentenceWords.length + currentSentenceWords.length;
+
+        if(!isNewSentence && !pastSpeed.isEmpty()) pastSpeed.remove(pastSpeed.size()-1);
+        currentSpeed = (currentSentenceWords.length) * 1.0 / ((System.currentTimeMillis() - currentBegin) *1.0 / 60000 + 0.001);
+        pastSpeed.add(currentSpeed);
+
+        boolean redrawRequired = false;
+
+        // 找到第一处差异
+        int firstDifferenceIndex = 0;
+        for (; firstDifferenceIndex < currentSentenceWords.length; firstDifferenceIndex++) {
+            if(firstDifferenceIndex >= lastSentenceWords.length){
+                redrawRequired = false;
+                break;
+            }
+            if(!currentSentenceWords[firstDifferenceIndex].equals(lastSentenceWords[firstDifferenceIndex])){
+                redrawRequired = true;
+                break;
+            }
+        }
+
+        if(isNewSentence) redrawRequired = false;
+
+
+        int redrawFromLineNumber = Math.max(wordNumbersOnScreen.size() - 1, 0);
+        int redrawFromWordNumber = Math.max(wordNumbersOnScreen.get(redrawFromLineNumber).size(), 0);
+        if(redrawRequired) {
+            // TODO: 处理首行
+            // 找到对应的屏幕上行位置
+            for (int j = wordNumbersOnScreen.size() - 1; j >= 0; j--) {
+                List<Pair<Integer, Integer>> currentLineWordNumbers = wordNumbersOnScreen.get(j);
+                for (int k = 0; k < currentLineWordNumbers.size(); k++) {
+                    if (currentLineWordNumbers.get(k).equals(new MutablePair<Integer, Integer>(currentSentence, firstDifferenceIndex))) {
+                        redrawFromLineNumber = j;
+                        redrawFromWordNumber = k;
+                    }
+                }
+            }
+        }
+
+        // 清除之前的排版数据
+        // 需要倒过来删除
+        if(redrawRequired) {
+            for (int i = linesOnScreen.get(redrawFromLineNumber).size() - 1; i >= redrawFromWordNumber; i--) {
+                linesOnScreen.get(redrawFromLineNumber).remove(i);
+                wordNumbersOnScreen.get(redrawFromLineNumber).remove(i);
+            }
+            for (int i = linesOnScreen.size() - 1; i >= redrawFromLineNumber + 1; i--) {
+                System.out.println("triggered!, newLine="+isNewSentence+" redraw="+redrawRequired);
+                List<String> removed1 = linesOnScreen.remove(i);
+                removed1.clear();
+                List<Pair<Integer, Integer>> removed2 = wordNumbersOnScreen.remove(i);
+                removed2.clear();
+            }
+        }
+
+        if(linesOnScreen.size()==0){
+            linesOnScreen.add(new ArrayList<>());
+            wordNumbersOnScreen.add(new ArrayList<>());
+        }
+
+        // 从对应位置开始重排
+        // 需要同时更新两个结构的数据
+        int writingLine = redrawFromLineNumber;
+//        System.out.println(writingLine);
+        for (int i = firstDifferenceIndex; i < currentSentenceWords.length; i++) {
+
+            String currentWord = currentSentenceWords[i];
+
+            // TODO: 重构写入过程
+            linesOnScreen.get(writingLine).add(currentWord);
+            wordNumbersOnScreen.get(writingLine).add(new MutablePair<>(currentSentence, i));
+            if(!isLineLegal(linesOnScreen.get(writingLine))){
+                linesOnScreen.get(writingLine).remove(linesOnScreen.get(writingLine).size()-1);
+                wordNumbersOnScreen.get(writingLine).remove(wordNumbersOnScreen.get(writingLine).size()-1);
+
+                writingLine++;
+                linesOnScreen.add(new ArrayList<>());
+                wordNumbersOnScreen.add(new ArrayList<>());
+
+                linesOnScreen.get(writingLine).add(currentWord);
+                wordNumbersOnScreen.get(writingLine).add(new MutablePair<>(currentSentence, i));
+            }
+
+        }
+
+        // 字幕展示
+        // 只显示最后两行
+
+        if(linesOnScreen.size() >= 2) {
+            line1 = String.join(" ", linesOnScreen.get(linesOnScreen.size() - 2));
+        }
+        if(linesOnScreen.size() >= 1){
+            line2 = String.join(" ", linesOnScreen.get(linesOnScreen.size() - 1));
+        }
+
+        System.out.println(redrawFromLineNumber + " " + redrawFromWordNumber);
+        System.out.println(linesOnScreen);
+        System.out.println(wordNumbersOnScreen);
+        System.out.println(line1);
+        System.out.println(line2);
+        System.out.println("----");
+//        in.nextInt();
+
+
+        // 删除顶行
+        if(linesOnScreen.size() > maxLines){
+            List<String> remove1 = linesOnScreen.remove(0);
+            remove1.clear();
+            List<Pair<Integer, Integer>> remove2 = wordNumbersOnScreen.remove(0);
+            remove2.clear();
+        }
+
+        // TODO: 这里可以优化，直接存数组
+        lastSentence = tr;
+    }
+
+    public String get(){
+        return line1 +"\n" + line2;
+    }
+}
